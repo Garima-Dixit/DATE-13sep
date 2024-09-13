ca143cf2932f4b80f1f9cc8691b8e0f555f1e898
@garima-dixit@gmail.com <dixitgarima0507@gmail.com>
2024-09-13 11:01:25 +0530

    upload first version

---
+++ controllers/usercontroller.js
@@ -0,0 +1,14 @@
+const users=require('../users.json');
+
+function getAllUsers(req,res){
+    try{
+        res.json(users)
+    }
+    catch(err){
+        
+    }
+
+}
+module.exports={
+    getAllUsers
+}
